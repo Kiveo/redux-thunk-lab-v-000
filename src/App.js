83458d6b206856bad22393635f9e689de693db29
Kiveo <mavericktesini@gmail.com>
2018-09-26 20:36:42 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import {fetchCats} from './actions/catActions';
+import {CatList} from './CatList';
+import { connect } from 'react-redux';
 
 class App extends Component {   
   
+  handleClick = (event) => {
+    event.preventDefault();
+    this.props.fetchCats();
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">CatBook</a>
+              <a href="#" onClick={event => this.handleClick(event)}>CatBook</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
@@ -19,6 +27,12 @@ class App extends Component {
 }
 
 
+function mapDispatchToProps(dispatch){
+  return { fetchCats: () => dispatch(fetchCats()) }
+}
+ 
+function mapStateToProps(state){
+  return {cats: state.cats}
+}
 
-export default App
-
+export default connect(mapStateToProps, mapDispatchToProps)(App)
